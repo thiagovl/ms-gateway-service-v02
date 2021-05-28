8f1dd175f260031c25bdb9247388c7771b741f3e
thiagovilarinholemes <lemes_vilarinho@yahoo.com.br>
2021-05-28 20:45:17 -0300

    Add cors configurations

---
+++ src/main/java/com/apimicroservice/config/CorsConfiguration.java
@@ -0,0 +1,19 @@
+package com.apimicroservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebFluxConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .exposedHeaders(HttpHeaders.SET_COOKIE);
+    }
+}
